9367a910c2f56b39ee46b344c8152f18ce24395a
Bond <shrekuu@gmail.com>
2024-11-13 17:56:09 +0800

    add asset prefix on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true, // Disable default image optimization
   },
+  assetPrefix: isProd ? "/nextjs-wagmi-demo/" : "",
+  basePath: isProd ? "/nextjs-wagmi-demo" : "",
   eslint: {
     // disable linting during build phase
     ignoreDuringBuilds: true,
